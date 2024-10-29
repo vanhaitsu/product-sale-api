2f6bf765d39d1cf81edb03882fb86295aeb49090
Ngoc Tuan <tuannnse170112@fpt.edu.vn>
2024-10-29 23:58:56 +0700

    feat: template send email

--- Repositories/Common/PaginationConstant.cs
+++ Repositories/Common/PaginationConstant.cs
@@ -2,6 +2,6 @@
 
     public static class PaginationConstant
     {
-        public const int DEFAULT_MIN_PAGE_SIZE = 10;
+        public const int DEFAULT_MIN_PAGE_SIZE = 1;
         public const int DEFAULT_MAX_PAGE_SIZE = 50;
     }
