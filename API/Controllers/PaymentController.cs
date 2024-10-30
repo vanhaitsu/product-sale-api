8d8abbe4129f4a1774123216488e7f872267d8bd
tinhcao <trancao134@gmail.com>
2024-10-30 21:35:54 +0700

    fix: payment

--- API/Controllers/PaymentController.cs
+++ API/Controllers/PaymentController.cs
@@ -5,6 +5,7 @@ using Services.Interfaces;
 using Services.Models.CartModels;
 using Services.Models.OrderModels;
 using Services.Services;
+using System.Web;
 
 namespace API.Controllers
 {
@@ -39,7 +40,42 @@ namespace API.Controllers
                 return BadRequest(ex.Message);
             }
         }
-            [HttpPut("adjustment-status")]
+
+        [HttpGet("redirect")]
+        public IActionResult RedirectWithUpdatedQuery(
+            [FromQuery] string vnp_Amount,
+            [FromQuery] string vnp_BankCode,
+            [FromQuery] string vnp_BankTranNo,
+            [FromQuery] string vnp_CardType,
+            [FromQuery] string vnp_OrderInfo,
+            [FromQuery] string vnp_PayDate,
+            [FromQuery] string vnp_ResponseCode,
+            [FromQuery] string vnp_TmnCode,
+            [FromQuery] string vnp_TransactionNo,
+            [FromQuery] string vnp_TransactionStatus,
+            [FromQuery] string vnp_TxnRef,
+            [FromQuery] string vnp_SecureHash)
+        {
+            var redirectUrl = $"productsale://productsale.shop/successPage/{vnp_TxnRef}";
+            var queryParams = HttpUtility.ParseQueryString(string.Empty);
+            queryParams["vnpAmount"] = vnp_Amount;
+            queryParams["vnpBankCode"] = vnp_BankCode;
+            queryParams["vnpBankTranNo"] = vnp_BankTranNo;
+            queryParams["vnpCardType"] = vnp_CardType;
+            queryParams["vnpOrderInfo"] = vnp_OrderInfo;
+            queryParams["vnpPayDate"] = vnp_PayDate;
+            queryParams["vnpResponseCode"] = vnp_ResponseCode;
+            queryParams["vnpTmnCode"] = vnp_TmnCode;
+            queryParams["vnpTransactionNo"] = vnp_TransactionNo;
+            queryParams["vnpTransactionStatus"] = vnp_TransactionStatus;
+            queryParams["vnpTxnRef"] = vnp_TxnRef;
+            queryParams["vnpSecureHash"] = vnp_SecureHash;
+            var finalRedirectUrl = $"{redirectUrl}?{queryParams}";
+            return Redirect(finalRedirectUrl);
+        }
+
+
+        [HttpPut("adjustment-status")]
             public async Task<IActionResult> UpdateOrderStatus([FromBody] UpdateOrderStatusModel updateOrderStatusModel)
             {
                 try
