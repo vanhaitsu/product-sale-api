9ae4f4faf3879b6ab5502cdfa9a336b1805ad85e
tinhcao <trancao134@gmail.com>
2024-10-30 15:07:28 +0700

    fix

--- Services/Services/PaymentGatewayService.cs
+++ Services/Services/PaymentGatewayService.cs
@@ -15,13 +15,18 @@ namespace Services.Services
         }
         public async Task<string> CreatePaymentUrlVnpay(PaymentInformationModel requestDto, HttpContext httpContext)
         {
+            var paymentUrl = "";
+            var vnPay = new PaymentInformationModel
+            {
+                AccountID = requestDto.AccountID,
+                Amount = requestDto.Amount,
+                CustomerName = requestDto.CustomerName,
+                BookingID = requestDto.BookingID
+            };
             var timeZoneById = TimeZoneInfo.FindSystemTimeZoneById(_configuration["TimeZoneId"]);
             var timeNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZoneById);
             var pay = new VNPayLibrary();
-
-            var baseReturnUrl = _configuration["Vnpay:ReturnUrl"];
-
-            var urlCallBack = $"{baseReturnUrl}/{requestDto.BookingID}";
+            var urlCallBack = $"{_configuration["Vnpay:ReturnUrl"]}/{requestDto.BookingID}";
 
             pay.AddRequestData("vnp_Version", _configuration["Vnpay:Version"]);
             pay.AddRequestData("vnp_Command", _configuration["Vnpay:Command"]);
@@ -32,44 +37,14 @@ namespace Services.Services
             pay.AddRequestData("vnp_IpAddr", pay.GetIpAddress(httpContext));
             pay.AddRequestData("vnp_Locale", _configuration["Vnpay:Locale"]);
             pay.AddRequestData("vnp_OrderInfo",
-                HttpUtility.UrlEncode($"Khach hang: {requestDto.CustomerName} thanh toan hoa don {requestDto.BookingID}"));
+                $"Khach hang: {requestDto.CustomerName} thanh toan hoa don {requestDto.BookingID}");
             pay.AddRequestData("vnp_OrderType", "other");
             pay.AddRequestData("vnp_ReturnUrl", urlCallBack);
             pay.AddRequestData("vnp_TxnRef", requestDto.BookingID);
-            var baseUrl = _configuration["Vnpay:BaseUrl"];
-            var hashSecret = _configuration["Vnpay:HashSecret"];
-            var rawPaymentUrl = pay.CreateRequestUrl(baseUrl, hashSecret);
-            var paymentUri = new Uri(rawPaymentUrl);
-            var queryParams = HttpUtility.ParseQueryString(paymentUri.Query);
-            var modifiedParams = new List<string>();
+            paymentUrl = pay.CreateRequestUrl(_configuration["Vnpay:BaseUrl"], _configuration["Vnpay:HashSecret"]);
 
-            foreach (string key in queryParams.Keys)
-            {
-                if (key != null)
-                {
-                    var camelCaseKey = ToCamelCase(key.Replace("vnp_", "vnp"));
-                    modifiedParams.Add($"{camelCaseKey}={HttpUtility.UrlEncode(queryParams[key])}");
-                }
-            }
-            var finalPaymentUrl = $"{baseUrl}?{string.Join("&", modifiedParams)}";
-            return finalPaymentUrl;
-        }
 
-        private string ToCamelCase(string str)
-        {
-            if (string.IsNullOrEmpty(str))
-                return str;
-
-            var words = str.Split('_');
-            var result = words[0].ToLower();
-
-            for (int i = 1; i < words.Length; i++)
-            {
-                if (!string.IsNullOrEmpty(words[i]))
-                    result += char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
-            }
-
-            return result;
+            return paymentUrl;
         }
     }
 }
